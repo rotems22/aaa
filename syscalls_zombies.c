2264a4a9f0aade62c769f137ce08c1ad29d2d1e0
rotems22 <rotems435@gmail.com>
2017-04-25 17:47:22 +0300

    Add files via upload

--- syscalls_zombies.c
+++ syscalls_zombies.c
@@ -25,43 +25,98 @@ int get_max_zombies(){
 	return curr->max_zombies;
 }
 
+int get_zombies_count(pid_t pid){
+	if (pid < 0){
+		errno=ESRCH;
+		return -1;
+	}
+	return zombies_counter;
+}
+
+pid_t get_zombie_pid(int n){
+	if (zombies_counter <= n){
+		errno=ESRCH;
+		return -1;
+	}
+	 if (innitialized==false){
+		errno=EINVAL;
+		return -1;
+	}
+	curr_task=find_task_by_pid(getpid());
+	list_t *curr_list;
+	curr_list=curr_task->zombies_list:
+	int counter=0;
+	list_for_each(CurNode,curr_list){
+			if(n==counter){
+				return list_entry(CurNode,pid_t,);
+			}
+			counter++;
+		
+	}
+
+
 
 
 int give_up_zombies(int n, pid_t adopter_pid){
-	if (n > get_zombies_count(){
+	if (n > get_zombies_count()){
 		errno=EINVAL;
 		return -1;
 	}
 	int new_count = n;
 	new_count += get_zombies_count(adopter_pid);
+
 	if (new_count>get_max_zombies(adopter_pid)){
 		errno=EINVAL;
 		return -1;
 	}
-	if (get_max_zombies() == -1){
+	if (innitialized==false){
 		errno=EINVAL;
 		return -1;
 	}
 	if (adopter_pid < 0){
 		errno=ESRCH;
-		return 0;
+		return -1;
 	}
 
 	pid_t curr_pid;
-	task_struct curr_task;
-	task_struct adopter_task;
+	list_t *pid_node;
+	task_struct *curr_task;
+	task_struct *adopter_task;
 	for(int i=0; i!=n; i++){
 		curr_pid=get_zombies_pid(i);
 		curr_task=find_task_by_pid(curr_pid);
-		adopter_task=find_task_by_pid();
-		adopter_task->counter++;
-		curr_task->counter--;
+		adopter_task=find_task_by_pid(adopter_pid);
+		
+		REMOVE_LINKS(curr_pid);
+		curr_task->p_pptr=adopter_task;
+		SET_LINKS(curr_pid);
 
-		**fix lists**
-
+		pid_node=find_pid_node(curr_pid);
+		list_add(pid_node,adopter_task->zombies_list);
+		list_del(pid_node);
+	
+		adopter_task->zombies_counter++;
+		curr_task->zombies_counter--;
 	}
-
-
-
+	return -1;
 
 }
+
+
+
+
+list_t* find_pid_node(pid_t pid){
+	curr_task=find_task_by_pid(pid);
+	list_t *curr_list;
+	curr_list=curr_task->zombies_list:
+
+	list_for_each(CurNode,curr_list){
+			if(pid==list_entry(CurNode,pid_t,));
+			return CurNode;
+	}
+
+	return 
+
+}
+
+
