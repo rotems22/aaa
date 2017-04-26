79b627b04495001dd398a96d4c74666c6df11f43
rotems22 <rotems435@gmail.com>
2017-04-26 16:05:26 +0300

    Add files via upload

--- added_to_sched.c
+++ added_to_sched.c
@@ -1,6 +1,12 @@
+/**add_to_sched.c**/
 int max_zombies;
 int zombies_counter;
 list_t zombies_list;
 bool innitialized=false;
 
-
+/**add_to_entry.s**/
+.long SYMBOL_NAME(sys_set_max_zombies)
+	.long SYMBOL_NAME(sys_get_max_zombies)
+	.long SYMBOL_NAME(sys_get_zombies_count)
+	.long SYMBOL_NAME(sys_get_zombie_pid)
+	.long SYMBOL_NAME(sys_give_up_zombies)
--- syscalls_zombies.c
+++ syscalls_zombies.c
@@ -1,4 +1,4 @@
-int set_max_zombies(int max_z, pid_t pid){
+int sys_set_max_zombies(int max_z, pid_t pid){
 	if (pid<o){
 		errno=EINVAL;
 		return -1;
@@ -15,7 +15,7 @@ int set_max_zombies(int max_z, pid_t pid){
 }
 
 
-int get_max_zombies(){
+int sys_get_max_zombies(){
 	task_struct curr;
 	curr=find_task_by_pid(getpid());
 	if (curr->innitalized!=true){
@@ -25,7 +25,7 @@ int get_max_zombies(){
 	return curr->max_zombies;
 }
 
-int get_zombies_count(pid_t pid){
+int sys_get_zombies_count(pid_t pid){
 	if (pid < 0){
 		errno=ESRCH;
 		return -1;
@@ -33,43 +33,65 @@ int get_zombies_count(pid_t pid){
 	return zombies_counter;
 }
 
-pid_t get_zombie_pid(int n){
-	if (zombies_counter <= n){
-		errno=ESRCH;
-		return -1;
-	}
-	 if (innitialized==false){
+pid_t sys_get_zombie_pid(int n){
+	if (n < 0){
 		errno=EINVAL;
 		return -1;
 	}
+
+	if (zombies_counter <= n){
+		errno=EINVAL;
+		return -1;
+	}
+	task_struct *curr_task;
 	curr_task=find_task_by_pid(getpid());
+	 if (curr->innitialized==false){
+		errno=EINVAL;
+		return -1;
+	}
+
 	list_t *curr_list;
 	curr_list=curr_task->zombies_list:
-	int counter=0;
+	int Ncount=0;
 	list_for_each(CurNode,curr_list){
-			if(n==counter){
-				return list_entry(CurNode,pid_t,);
-			}
-			counter++;
-		
+		if(n==Ncount){
+			return list_entry(CurNode,pid_t,**********);
+		}
+		Ncount++;
+	}
+		return list_entry(curr_list,pid_t,**********);
 	}
 
 
 
 
-int give_up_zombies(int n, pid_t adopter_pid){
+int sys_give_up_zombies(int n, pid_t adopter_pid){
+	pid_t curr_pid;
+	list_t *pid_node;
+	task_struct *curr_task;
+	task_struct *adopter_task;
+	adopter_task=find_task_by_pid(adopter_pid);
+
+	if (n < 0){
+		errno=EINVAL;
+		return -1;
+	}
+
 	if (n > get_zombies_count()){
 		errno=EINVAL;
 		return -1;
 	}
 	int new_count = n;
 	new_count += get_zombies_count(adopter_pid);
-
-	if (new_count>get_max_zombies(adopter_pid)){
-		errno=EINVAL;
-		return -1;
+	if(adopter_pid!=getpid()){
+		if (new_count>get_max_zombies(adopter_pid)){
+			errno=EINVAL;
+			return -1;
+		}
 	}
-	if (innitialized==false){
+	
+
+	if (adopter_task->innitialized==false){
 		errno=EINVAL;
 		return -1;
 	}
@@ -78,14 +100,9 @@ int give_up_zombies(int n, pid_t adopter_pid){
 		return -1;
 	}
 
-	pid_t curr_pid;
-	list_t *pid_node;
-	task_struct *curr_task;
-	task_struct *adopter_task;
 	for(int i=0; i!=n; i++){
 		curr_pid=get_zombies_pid(i);
 		curr_task=find_task_by_pid(curr_pid);
-		adopter_task=find_task_by_pid(adopter_pid);
 		
 		REMOVE_LINKS(curr_pid);
 		curr_task->p_pptr=adopter_task;
@@ -98,25 +115,23 @@ int give_up_zombies(int n, pid_t adopter_pid){
 		adopter_task->zombies_counter++;
 		curr_task->zombies_counter--;
 	}
-	return -1;
-
+	return 0;
 }
-
-
-
 
 list_t* find_pid_node(pid_t pid){
 	curr_task=find_task_by_pid(pid);
 	list_t *curr_list;
-	curr_list=curr_task->zombies_list:
+	curr_list = &(curr_task->zombies_list):
 
 	list_for_each(CurNode,curr_list){
-			if(pid==list_entry(CurNode,pid_t,));
+			if(pid==list_entry(CurNode,pid_t,**********));
 			return CurNode;
 	}
-
-	return 
-
+	return curr_list;
 }
+
+
+
+
 
 
